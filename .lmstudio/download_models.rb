0c56745ab3ca1c407c81ed2d61194f4e8d8e6528
Mihai Catalin Florescu <fratelemk@outlook.com>
2026-05-03 10:30:03 +0100

    Add script to download models using LM Studio API

---
+++ .lmstudio/download_models.rb
@@ -0,0 +1,26 @@
+require 'net/http'
+require 'uri'
+require 'json'
+
+uri = URI.parse("http://localhost:1234/api/v1/models/download")
+
+http = Net::HTTP.new(uri.host, uri.port)
+request = Net::HTTP::Post.new(uri)
+
+File.foreach("model_catalog") do |line|
+  stripped = line.strip
+  next if stripped.empty? || stripped.start_with?("#")
+
+  request = Net::HTTP::Post.new(uri)
+  request["Content-Type"] = "application/json"
+
+  request.body = {
+    model: stripped
+  }.to_json
+
+  response = http.request(request)
+
+  puts "Model: #{stripped}"
+  puts "Body: #{response.body}"
+  puts "-" * 40
+end
