d94e3178e7229f21a8a353499dc51f295d1004c7
Mihai Catalin Florescu <fratelemk@outlook.com>
2026-07-11 16:38:39 +0100

    Refactor model download script to use constant path

--- .lmstudio/download_models.rb
+++ .lmstudio/download_models.rb
@@ -1,25 +1,27 @@
 require 'net/http'
 require 'uri'
 require 'json'
-
+ 
+CATALOG_PATH = File.join(__dir__, "model_catalog")
+ 
 uri = URI.parse("http://localhost:1234/api/v1/models/download")
-
+ 
 http = Net::HTTP.new(uri.host, uri.port)
 request = Net::HTTP::Post.new(uri)
-
-File.foreach("model_catalog") do |line|
+ 
+File.foreach(CATALOG_PATH) do |line|
   stripped = line.strip
   next if stripped.empty? || stripped.start_with?("#")
-
+ 
   request = Net::HTTP::Post.new(uri)
   request["Content-Type"] = "application/json"
-
+ 
   request.body = {
     model: stripped
   }.to_json
-
+ 
   response = http.request(request)
-
+ 
   puts "Model: #{stripped}"
   puts "Body: #{response.body}"
   puts "-" * 40
