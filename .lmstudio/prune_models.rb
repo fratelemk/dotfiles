8b8edd6e9ee25f4efd1e46f58be3405cb97a05ae
Mihai Catalin Florescu <fratelemk@outlook.com>
2026-07-11 16:40:43 +0100

    Refactor model catalog path handling in prune_models.rb

--- .lmstudio/prune_models.rb
+++ .lmstudio/prune_models.rb
@@ -1,21 +1,22 @@
 require 'fileutils'
 require 'set'
-
+ 
 MODELS_DIR = File.expand_path("~/.lmstudio/models")
-
+CATALOG_PATH = File.join(__dir__, "model_catalog")
+ 
 keep = Set.new
-File.foreach("model_catalog") do |line|
+File.foreach(CATALOG_PATH) do |line|
   stripped = line.strip
   next if stripped.empty? || stripped.start_with?("#")
   keep << stripped
 end
-
+ 
 Dir.glob(File.join(MODELS_DIR, "*", "*")).each do |path|
   next unless Dir.exist?(path)
-
+ 
   key = path.delete_prefix("#{MODELS_DIR}/")
   next if keep.include?(key)
-
+ 
   puts "Removing: #{key}"
   puts "Path: #{path}"
   FileUtils.rm_rf(path)
